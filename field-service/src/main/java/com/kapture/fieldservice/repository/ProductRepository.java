ae42fd7f6f8a9282011c3f1d76940418be3ae33f
Gourav_Arora <gourav.arora@kapturecrm.com>
2023-10-28 01:58:42 +0530

    Update ProductRepository.java

--- field-service/src/main/java/com/kapture/fieldservice/repository/ProductRepository.java
+++ field-service/src/main/java/com/kapture/fieldservice/repository/ProductRepository.java
@@ -1,6 +1,9 @@
 package com.kapture.fieldservice.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.kapture.fieldservice.object.Product;
@@ -8,4 +11,6 @@ import com.kapture.fieldservice.object.Product;
 @Repository
 public interface ProductRepository extends MongoRepository<Product, Integer>{
 
+	@Query("{ 'cmId': ?0}")
+	public List<Product> getByCmId(int cmId);
 }
