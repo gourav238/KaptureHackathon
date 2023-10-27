120fb6530627f893e5fa375399a0e1cc3818db9f
Gourav_Arora <gourav.arora@kapturecrm.com>
2023-10-28 05:26:14 +0530

    Update SupplierRepository.java

--- field-service/src/main/java/com/kapture/fieldservice/repository/SupplierRepository.java
+++ field-service/src/main/java/com/kapture/fieldservice/repository/SupplierRepository.java
@@ -14,6 +14,6 @@ public interface SupplierRepository extends MongoRepository<Supplier, String>{
 	@Query("{ \"cmId\": ?0, \"isEnable\": true}")
 	public List<Supplier> getByCmId(int cmId);
 	
-	@Query("{ \"id\": ?0 }")
+	@Query("{ \"_id\": ?0 }")
 	public Supplier getById(String id);
 }
