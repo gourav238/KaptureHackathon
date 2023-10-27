fbbfe50026964d3acf63d0844c5dd5b371a2b831
Gourav_Arora <gourav.arora@kapturecrm.com>
2023-10-27 23:24:22 +0530

    Create PartnerRepository.java

---
+++ field-service/src/main/java/com/kapture/fieldservice/repository/PartnerRepository.java
@@ -0,0 +1,11 @@
+package com.kapture.fieldservice.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.kapture.fieldservice.object.Partner;
+
+@Repository
+public interface PartnerRepository extends MongoRepository<Partner, Integer>{
+
+}
