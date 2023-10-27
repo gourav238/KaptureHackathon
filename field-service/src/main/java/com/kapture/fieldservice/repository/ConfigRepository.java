2f3f4e84f9ff6bdcc2681650371cece8aac3abc1
Gourav_Arora <gourav.arora@kapturecrm.com>
2023-10-28 02:28:57 +0530

    .

--- field-service/src/main/java/com/kapture/fieldservice/repository/ConfigRepository.java
+++ field-service/src/main/java/com/kapture/fieldservice/repository/ConfigRepository.java
@@ -1,6 +1,7 @@
 package com.kapture.fieldservice.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.kapture.fieldservice.object.Config;
@@ -8,6 +9,7 @@ import com.kapture.fieldservice.object.Config;
 @Repository
 public interface ConfigRepository extends MongoRepository<Config, Integer>{
 
+	@Query("{ \"cmId\": ?0, \"configName\": ?1}")
     public Config findByCmIdAndConfigName(int cmId, String configName);
 
 }
