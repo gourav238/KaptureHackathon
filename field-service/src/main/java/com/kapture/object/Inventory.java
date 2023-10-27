8120f078eb1d095ebd33d295963d06d68a6c3a25
Gourav Arora <66515526+gourav238@users.noreply.github.com>
2023-10-27 21:52:37 +0530

    Add files via upload

---
+++ field-service/src/main/java/com/kapture/object/Inventory.java
@@ -0,0 +1,45 @@
+package com.kapture.object;
+
+import java.util.Calendar;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import lombok.Data;
+
+@Data
+@Document(collection = "inventory")
+public class Inventory {
+
+	@Id
+	@Field
+	private int id;
+	
+	@Field
+	@Indexed
+	private int cmId;
+	
+	@Field
+	private int ascId;
+	
+	@Field
+	@Indexed
+	private int productId;
+	
+	@Field
+	private int quantity;
+	
+	@Field
+	private JsonNode otherDetail;
+	
+	@Field
+	private Calendar createdDate;
+	
+	@Field
+	@Indexed
+	private Calendar lastUpdatedDate;
+}
---
+++ field-service/src/main/java/com/kapture/object/Product.java
@@ -0,0 +1,63 @@
+package com.kapture.object;
+
+import java.util.Calendar;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import lombok.Data;
+
+@Data
+@Document(collection = "product")
+public class Product {
+
+	@Id
+	@Field
+	private int id;
+	
+	@Field
+	@Indexed
+	private int cmId;
+	
+	@Field
+	@Indexed
+	private String parentCategory;
+	
+	@Field
+	private String name;
+	
+	@Field
+	@Indexed
+	private String skuCode;
+	
+	@Field
+	@Indexed
+	private String description;
+	
+	@Field
+	private JsonNode config;		//jsonobject other details
+	
+	@Field
+	@Indexed
+	private int supplierId;
+	
+	@Field
+	private double price;
+	
+	@Field
+	private JsonNode tax;			//jsonobject based on country
+	
+	@Field
+	private int warranty;		//number of days
+	
+	@Field
+	private Calendar createdDate;
+	
+	@Field
+	@Indexed
+	private Calendar lastUpdatedDate;
+}
