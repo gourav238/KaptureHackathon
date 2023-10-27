d94b13031a551388d3ceffd88527d284c910b1a7
Gourav Arora <66515526+gourav238@users.noreply.github.com>
2023-10-27 22:03:13 +0530

    Add files via upload

---
+++ field-service/src/main/java/com/kapture/fieldservice/object/ASC.java
@@ -0,0 +1,43 @@
+package com.kapture.fieldservice.object;
+
+import java.util.Calendar;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import lombok.Data;
+
+@Data
+public class ASC {
+
+	@Id
+	@Field
+	private int id;
+	
+	@Field
+	@Indexed
+	private int cmId;
+	
+	@Field
+	private JsonNode address;
+	
+	@Field
+	private String name;
+	
+	@Field
+	@Indexed
+	private int externalId;
+	
+	@Field
+	private JsonNode orderDetail;
+	
+	@Field
+	private Calendar createdDate;
+	
+	@Field
+	private Calendar lastUpdatedDate;
+
+}
