5b02636f2b36bdcdc09694d19f51ba1b0deb8bb3
Gourav_Arora <gourav.arora@kapturecrm.com>
2023-10-28 06:31:48 +0530

    -

--- field-service/src/main/java/com/kapture/fieldservice/object/SalesOrder.java
+++ field-service/src/main/java/com/kapture/fieldservice/object/SalesOrder.java
@@ -1,14 +1,14 @@
 package com.kapture.fieldservice.object;
 
 
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+import org.springframework.data.mongodb.core.mapping.MongoId;
 
 @Document
 public class SalesOrder {
-    @Id
-    @Field
+    @MongoId(FieldType.OBJECT_ID)
     private String id;
     @Field
     private int cmId;
