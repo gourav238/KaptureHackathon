5e5c6eeb89d87b384b730f3098fc3a05e5abeebd
BhushanKumarSingh <46242228+BhushanKumarSingh@users.noreply.github.com>
2023-10-28 06:37:38 +0530

    Update ProductController.java

--- field-service/src/main/java/com/kapture/fieldservice/controller/ProductController.java
+++ field-service/src/main/java/com/kapture/fieldservice/controller/ProductController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,7 +28,7 @@ public class ProductController {
         return productService.saveProduct(requestBody, request);
     }
 
-    @PostMapping("/get")
+    @GetMapping("/get")
     public ResponseEntity< ? > getProductListByCmId() {
         return productService.getProductList();
     }
